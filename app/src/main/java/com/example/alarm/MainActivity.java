79d3a95c72cc27dd69e085381e68634afddf2760
Jenny Araneta <mjmaraneta@tip.edu.ph>
2023-05-07 02:23:53 +0700

    Added functions to Log In.

--- app/src/main/java/com/example/alarm/MainActivity.java
+++ app/src/main/java/com/example/alarm/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.alarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
     }
 }
--- app/src/main/java/com/example/alarm/logIn.java
+++ app/src/main/java/com/example/alarm/logIn.java
@@ -2,13 +2,48 @@ package com.example.alarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class logIn extends AppCompatActivity {
+
+
+    EditText loginText, passwordText;
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        loginText = (EditText) findViewById(R.id.emailAddress);
+        passwordText = (EditText) findViewById(R.id.password);
+        btnLogin = (Button) findViewById(R.id.loginButton);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String username = loginText. getText().toString();
+                String password = passwordText. getText(). toString();
+
+                if(username.equals("beary") && password.equals("gud"))
+                {
+                    Toast.makeText(logIn.this, "Welcome to Alarm", Toast.LENGTH_SHORT).show();
+                    Intent intent= new Intent(getApplicationContext(),homePage.class);
+                    startActivity(intent);
+                }else
+                {
+                    Toast.makeText(logIn.this, "Invalid Credentials.", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
     }
 }
